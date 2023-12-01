3fbe32233257257332f08d98e022c074a7d736ff
Daniel Olczyk <contact@dolczyk.rocks>
2023-12-01 23:23:58 +0100

    Add first day part one solution

---
+++ Day1/Program.fs
@@ -0,0 +1,27 @@
+﻿open System
+open System.IO
+
+let lines = File.ReadAllLines("input.txt")
+
+let mutable finalNumbers = []
+for line in lines do
+    let mutable result = ""
+    let mutable numbers = []
+    let chars = line.ToCharArray()
+    
+    for char in chars do
+        let n = 
+            match Int32.TryParse(char.ToString()) with
+            | true, number -> number
+            | _ -> 0
+        if n > 0 then
+            numbers <- n :: numbers
+        
+    if numbers.Length = 1 then
+        result <- $"{numbers.Head}{numbers.Head}"
+    else
+        result <- $"{numbers[numbers.Length - 1]}{numbers[0]}"
+    finalNumbers <- result :: finalNumbers
+    
+let a = finalNumbers |> List.map Int32.Parse |> List.sum
+printf "%d" a
